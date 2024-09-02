b49add3cb80eeb340b97d8d29964f8d9509d1944
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-09-02 18:49:30 +0200

    Fixed help command display

--- src/main/kotlin/pl/syntaxdevteam/commands/PunisherXCommands.kt
+++ src/main/kotlin/pl/syntaxdevteam/commands/PunisherXCommands.kt
@@ -16,27 +16,29 @@ class PunishesXCommands(private val plugin: PunisherX) : BasicCommand {
             when {
                 args[0].equals("help", ignoreCase = true) -> {
                     if (stack.sender.hasPermission("punisherx.help")) {
-                        stack.sender.sendRichMessage("\n<gray>#############################################\n#\n#  <gold>Available commands for " + pluginMeta.name + ":\n" +
+                        stack.sender.sendRichMessage("\n<gray>#################################################\n#\n#  <gold>Available commands for " + pluginMeta.name + ":\n" +
                                 "<gray>#\n" +
                                 "<gray>#  <gold>/punisherx help <gray>- <white>Displays this prompt. \n" +
                                 "<gray>#  <gold>/punisherx version <gray>- <white>Shows plugin info. \n" +
                                 "<gray>#  <gold>/punisherx reload <gray>- <white>Reloads the configuration file\n" +
-                                "<gray>#  <gold>/ban <player> (time) <reason> <gray>- <white>Bans a player \n" +
-                                "<gray>#  <gold>/banip <player/ip> (time) <reason> <gray>- <white>Bans a player's IP\n" +
-                                "<gray>#  <gold>/unban <player/ip> <gray>- <white>Unbans a player\n" +
                                 "<gray>#  <gold>/warn <player> (time) <reason> <gray>- <white>Warns a player\n" +
                                 "<gray>#  <gold>/unwarn <player> <gray>- <white>Removes a player's warning\n" +
                                 "<gray>#  <gold>/mute <player> (time) <reason> <gray>- <white>Mutes a player\n" +
                                 "<gray>#  <gold>/unmute <player> <gray>- <white>Unmutes a player\n" +
+                                "<gray>#  <gold>/ban <player> (time) <reason> <gray>- <white>Bans a player \n" +
+                                "<gray>#  <gold>/banip <player/ip> (time) <reason> <gray>- <white>Bans a player's IP\n" +
+                                "<gray>#  <gold>/unban <player/ip> <gray>- <white>Unbans a player\n" +
+                                "<gray>#  <gold>/check <player> <all/warn/mute/ban>\n" +
+                                "<gray>#      - <white>Checks and displays the punishments of a given player\n" +
                                 "<gray>#\n" +
-                                "<gray>#\n#############################################")
+                                "<gray>#\n#################################################")
                     } else {
                         stack.sender.sendRichMessage("<red>You do not have permission to use this command.</red>")
                     }
                 }
                 args[0].equals("version", ignoreCase = true) -> {
                     if (stack.sender.hasPermission("punisherx.version")) {
-                        stack.sender.sendRichMessage("\n<gray>#############################################\n#\n#   <gold>→ <bold>" + pluginMeta.name + "</bold> ←\n<gray>#   <white>Author: <bold><gold>" + pdf.authors + "</gold></bold>\n<gray>#   <white>Website: <bold><gold><click:open_url:'" + pdf.website + "'>"  + pdf.website + "</click></gold></bold>\n<gray>#   <white>Version: <bold><gold>" + pluginMeta.version + "</gold></bold><gray>\n#\n#############################################")
+                        stack.sender.sendRichMessage("\n<gray>#################################################\n#\n#   <gold>→ <bold>" + pluginMeta.name + "</bold> ←\n<gray>#   <white>Author: <bold><gold>" + pdf.authors + "</gold></bold>\n<gray>#   <white>Website: <bold><gold><click:open_url:'" + pdf.website + "'>"  + pdf.website + "</click></gold></bold>\n<gray>#   <white>Version: <bold><gold>" + pluginMeta.version + "</gold></bold><gray>\n#\n#################################################")
                     } else {
                         stack.sender.sendRichMessage("<red>You do not have permission to use this command.</red>")
                     }
