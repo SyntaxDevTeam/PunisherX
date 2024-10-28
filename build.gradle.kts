9e3b46bd7a7e4da1b2fa3bd4d4ec7444d847a13f
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-10-28 19:58:58 +0100

    Dependency management has been reorganized

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "pl.syntaxdevteam.punisher"
-version = "1.1.1-DEV"
+version = "1.1.2-DEV"
 
 repositories {
     mavenCentral()
@@ -17,20 +17,19 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0-Beta2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
     compileOnly("dev.folia:folia-api:1.20.1-R0.1-SNAPSHOT")
-
-    implementation("org.eclipse.aether:aether-api:1.1.0")
-    implementation("org.yaml:snakeyaml:2.3")
-
-    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.0")
-    compileOnly("org.postgresql:postgresql:42.7.4")
-    compileOnly("com.h2database:h2:2.3.232")
+    compileOnly("org.eclipse.aether:aether-api:1.1.0")
+    compileOnly("org.yaml:snakeyaml:2.3")
     compileOnly("com.google.code.gson:gson:2.11.0")
     compileOnly("com.maxmind.geoip2:geoip2:4.2.1")
     compileOnly("org.apache.ant:ant:1.10.15")
+    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.0")
+    compileOnly("org.postgresql:postgresql:42.7.4")
+    compileOnly("com.h2database:h2:2.3.232")
     compileOnly("com.zaxxer:HikariCP:6.0.0")
 }
 
