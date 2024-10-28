0184a3a6197c9f64aef302e6fd42b8763f96ff09
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-10-28 19:56:45 +0100

    * Improved UUID reading class * Added alternative API

--- src/main/kotlin/pl/syntaxdevteam/punisher/helpers/UUIDManager.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/helpers/UUIDManager.kt
@@ -20,18 +20,20 @@ class UUIDManager(private val plugin: PunisherX) {
         if (player != null) {
             return player.uniqueId
         }
-
         val offlinePlayer: OfflinePlayer = Bukkit.getOfflinePlayer(playerName)
         if (offlinePlayer.hasPlayedBefore()) {
             return offlinePlayer.uniqueId
         }
-
         val uuid = fetchUUIDFromAPI(playerName)
-        return uuid ?: generateOfflineUUID(playerName)
+            ?: fetchUUIDFromPlayerDB(playerName)
+            ?: fetchUUIDFromNameMC(playerName)
+            ?: generateOfflineUUID(playerName)
+        return uuid
     }
 
     private fun fetchUUIDFromAPI(playerName: String): UUID? {
-        val uri = URI("https://api.mojang.com/users/profiles/minecraft/$playerName")
+        val player: Player? = Bukkit.getPlayer(playerName)
+        val uri = URI("https://api.mojang.com/users/profiles/minecraft/${player?.name}")
         return try {
             val connection = uri.toURL().openConnection() as HttpURLConnection
             connection.requestMethod = "GET"
@@ -45,12 +47,77 @@ class UUIDManager(private val plugin: PunisherX) {
                 reader.close()
 
                 val uuid = parseUUIDFromResponse(response)
+                plugin.logger.debug("parseUUIDFromResponse(response): $uuid")
                 if (uuid != null) {
                     activeUUIDs[playerName.lowercase(Locale.getDefault())] = uuid
                 }
                 uuid
             } else {
                 plugin.logger.err("Failed to fetch UUID from API. Response code: ${connection.responseCode}")
+                player?.uniqueId
+            }
+        } catch (e: Exception) {
+            plugin.logger.err("Error: $e")
+            player?.uniqueId
+        }
+    }
+
+    private fun fetchUUIDFromPlayerDB(playerName: String): UUID? {
+        val uri = URI("https://playerdb.co/api/player/minecraft/$playerName")
+        return try {
+            val connection = uri.toURL().openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connect()
+            plugin.logger.debug("API Response Code: ${connection.responseCode}")
+            if (connection.responseCode == 200) {
+                val reader = InputStreamReader(connection.inputStream)
+                val response = reader.readText()
+                reader.close()
+                val jsonObject = gson.fromJson(response, JsonObject::class.java)
+                val rawUUID = jsonObject.getAsJsonObject("data").getAsJsonObject("player").get("id").asString
+                plugin.logger.debug("Raw UUID from PlayerDB API: $rawUUID")
+                val uuid = UUID.fromString(rawUUID.replaceFirst(
+                    "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)".toRegex(),
+                    "$1-$2-$3-$4-$5"
+                ))
+                if (uuid != null) {
+                    activeUUIDs[playerName.lowercase(Locale.getDefault())] = uuid
+                }
+                uuid
+            } else {
+                plugin.logger.err("Failed to fetch UUID from PlayerDB API. Response code: ${connection.responseCode}")
+                null
+            }
+        } catch (e: Exception) {
+            plugin.logger.err("Error: $e")
+            null
+        }
+    }
+
+    private fun fetchUUIDFromNameMC(playerName: String): UUID? {
+        val uri = URI("https://api.namemc.com/profile/$playerName")
+        return try {
+            val connection = uri.toURL().openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connect()
+            plugin.logger.debug("API Response Code: ${connection.responseCode}")
+            if (connection.responseCode == 200) {
+                val reader = InputStreamReader(connection.inputStream)
+                val response = reader.readText()
+                reader.close()
+                val jsonObject = gson.fromJson(response, JsonObject::class.java)
+                val rawUUID = jsonObject.get("id").asString
+                plugin.logger.debug("Raw UUID from NameMC API: $rawUUID")
+                val uuid = UUID.fromString(rawUUID.replaceFirst(
+                    "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)".toRegex(),
+                    "$1-$2-$3-$4-$5"
+                ))
+                if (uuid != null) {
+                    activeUUIDs[playerName.lowercase(Locale.getDefault())] = uuid
+                }
+                uuid
+            } else {
+                plugin.logger.err("Failed to fetch UUID from NameMC API. Response code: ${connection.responseCode}")
                 null
             }
         } catch (e: Exception) {
@@ -76,7 +143,7 @@ class UUIDManager(private val plugin: PunisherX) {
 
     private fun generateOfflineUUID(playerName: String): UUID {
         val offlineUUID = UUID.nameUUIDFromBytes("OfflinePlayer:$playerName".toByteArray())
-        println("Generated offline UUID for $playerName: $offlineUUID")
+        plugin.logger.debug("Generated offline UUID for $playerName: $offlineUUID")
         return offlineUUID
     }
 }
