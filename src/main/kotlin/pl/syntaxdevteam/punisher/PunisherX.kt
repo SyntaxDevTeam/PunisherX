e0795ad496c8438bccf1c062a962797c21370777
SyntaxDevTeam <syntaxerr85@gmail.com>
2025-01-13 20:25:32 +0100

    Added partial documentation to the code

--- src/main/kotlin/pl/syntaxdevteam/punisher/PunisherX.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/PunisherX.kt
@@ -14,13 +14,15 @@ import pl.syntaxdevteam.punisher.players.*
 import java.io.File
 import java.util.*
 
-
-@Suppress("UnstableApiUsage", "unused")
+/**
+ * Main class for the PunisherX plugin.
+ * This class handles the initialization and management of the plugin's components.
+ */
+@Suppress("UnstableApiUsage")
 class PunisherX : JavaPlugin(), Listener {
     private val configHandler by lazy { ConfigHandler(this) }
     private val config: FileConfiguration = configHandler.getConfig()
     var logger: Logger = Logger(this, config.getBoolean("debug"))
-    private val language = config.getString("language") ?: "EN"
     private lateinit var pluginsManager: PluginManager
     private lateinit var statsCollector: StatsCollector
     lateinit var databaseHandler: DatabaseHandler
@@ -34,6 +36,10 @@ class PunisherX : JavaPlugin(), Listener {
     private lateinit var commandManager: CommandManager
     lateinit var cache: PunishmentCache
 
+    /**
+     * Called when the plugin is enabled.
+     * Initializes the configuration, database, handlers, events, and commands.
+     */
     override fun onEnable() {
         setupConfig()
         setupDatabase()
@@ -43,26 +49,43 @@ class PunisherX : JavaPlugin(), Listener {
         checkForUpdates()
     }
 
+    /**
+     * Called when the plugin is reloaded.
+     * Reloads the configuration and reinitializes the database connection.
+     */
     fun onReload() {
         reloadMyConfig()
     }
 
+    /**
+     * Called when the plugin is disabled.
+     * Closes the database connection and unregisters events.
+     */
     override fun onDisable() {
         databaseHandler.closeConnection()
         AsyncChatEvent.getHandlerList().unregister(this as Plugin)
         logger.err(this.pluginMeta.name + " " + this.pluginMeta.version + " has been disabled ☹️")
     }
 
+    /**
+     * Sets up the plugin configuration.
+     */
     private fun setupConfig() {
         configHandler.updateConfig()
     }
 
+    /**
+     * Sets up the database connection and creates necessary tables.
+     */
     private fun setupDatabase() {
         databaseHandler = DatabaseHandler(this)
         databaseHandler.openConnection()
         databaseHandler.createTables()
     }
 
+    /**
+     * Initializes various handlers used by the plugin.
+     */
     private fun setupHandlers() {
         messageHandler = MessageHandler(this).apply { initial() }
         timeHandler = TimeHandler(this)
@@ -70,30 +93,46 @@ class PunisherX : JavaPlugin(), Listener {
         geoIPHandler = GeoIPHandler(this)
         pluginsManager = PluginManager(this)
         cache = PunishmentCache(this)
-
     }
 
+    /**
+     * Registers the plugin commands.
+     */
     private fun registerCommands(){
         commandManager = CommandManager(this)
         commandManager.registerCommands()
     }
 
+    /**
+     * Registers the plugin events.
+     */
     private fun registerEvents() {
         server.pluginManager.registerEvents(playerIPManager, this)
         server.pluginManager.registerEvents(PunishmentChecker(this), this)
     }
 
+    /**
+     * Checks for updates to the plugin.
+     */
     private fun checkForUpdates() {
         statsCollector = StatsCollector(this)
         updateChecker = UpdateChecker(this)
         updateChecker.checkForUpdates()
     }
 
+    /**
+     * Retrieves the plugin file.
+     *
+     * @return The plugin file.
+     */
     fun getPluginFile(): File {
         return this.file
     }
 
-    fun reloadMyConfig() {
+    /**
+     * Reloads the plugin configuration and reinitializes the database connection.
+     */
+    private fun reloadMyConfig() {
         databaseHandler.closeConnection()
         try {
             configHandler.reloadConfig()
@@ -104,9 +143,13 @@ class PunisherX : JavaPlugin(), Listener {
         databaseHandler = DatabaseHandler(this)
         databaseHandler.openConnection()
         databaseHandler.createTables()
-
     }
 
+    /**
+     * Retrieves the server name from the server.properties file.
+     *
+     * @return The server name, or "Unknown Server" if not found.
+     */
     fun getServerName(): String {
         val properties = Properties()
         val file = File("server.properties")
@@ -124,6 +167,17 @@ class PunisherX : JavaPlugin(), Listener {
         return "Unknown Server"
     }
 
+    /**
+     * API for RewarderX plugin
+     * Retrieves the punishment history for a given player.
+     *
+     * This method fetches the UUID of the player using the uuidManager and then calls the
+     * getLastTenPunishments method to retrieve the last ten punishments for the player.
+     *
+     * @param player The player whose punishment history is to be retrieved.
+     * @return A list of PunishmentData objects representing the player's punishment history.
+     */
+    @Suppress("unused")
     fun getPunishmentHistory(player: Player): List<PunishmentData> {
         val uuid = uuidManager.getUUID(player.name)
         return databaseHandler.getLastTenPunishments(uuid.toString())
