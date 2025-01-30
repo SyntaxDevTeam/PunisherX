6bd7ee4f0997cac7c9d35ef3b42a096b26b54464
SyntaxDevTeam <syntaxerr85@gmail.com>
2025-01-30 18:36:05 +0100

    Added missing documentations

--- src/main/kotlin/pl/syntaxdevteam/punisher/databases/PunishmentData.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/databases/PunishmentData.kt
@@ -1,3 +1,13 @@
 package pl.syntaxdevteam.punisher.databases
 
+/**
+ * Data class for punishment data
+ *
+ * @param id The id of the punishment
+ * @param uuid The uuid of the punished player
+ * @param type The type of the punishment
+ * @param reason The reason of the punishment
+ * @param start The start of the punishment
+ * @param end The end of the punishment
+ */
 data class PunishmentData(val id: Int, val uuid: String, val type: String, val reason: String, val start: Long, val end: Long)
