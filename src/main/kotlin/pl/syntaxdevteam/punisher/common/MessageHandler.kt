5912611426da90306ef90a06e5f530244c3bdc72
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-02 06:10:20 +0100

    Added full conversion of text formatting to MiniMessage.

--- src/main/kotlin/pl/syntaxdevteam/punisher/common/MessageHandler.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/common/MessageHandler.kt
@@ -165,4 +165,44 @@ class MessageHandler(private val plugin: PunisherX) {
     fun getPlainText(component: Component): String {
         return PlainTextComponentSerializer.plainText().serialize(component)
     }
+
+    private fun convertLegacyToMiniMessage(message: String): String {
+        return message
+            .replace("&0", "<black>") // Czarny
+            .replace("&1", "<dark_blue>") // Ciemnoniebieski
+            .replace("&2", "<dark_green>") // Ciemnozielony
+            .replace("&3", "<dark_aqua>") // Ciemny turkus
+            .replace("&4", "<dark_red>") // Ciemnoczerwony
+            .replace("&5", "<dark_purple>") // Ciemnofioletowy
+            .replace("&6", "<gold>") // Złoty
+            .replace("&7", "<gray>") // Szary
+            .replace("&8", "<dark_gray>") // Ciemnoszary
+            .replace("&9", "<blue>") // Niebieski
+            .replace("&a", "<green>") // Zielony
+            .replace("&b", "<aqua>") // Turkusowy
+            .replace("&c", "<red>") // Czerwony
+            .replace("&d", "<light_purple>") // Jasnofioletowy
+            .replace("&e", "<yellow>") // Żółty
+            .replace("&f", "<white>") // Biały
+            .replace("&k", "<obfuscated>") // Efekt zakodowanego tekstu
+            .replace("&l", "<bold>") // Pogrubienie
+            .replace("&m", "<strikethrough>") // Przekreślenie
+            .replace("&n", "<underlined>") // Podkreślenie
+            .replace("&o", "<italic>") // Kursywa
+            .replace("&r", "<reset>") // Resetowanie stylów
+    }
+
+    private fun convertHexToMiniMessage(message: String): String {
+        return message.replace("&#([a-fA-F0-9]{6})".toRegex()) {
+            val hex = it.groupValues[1]
+            "<#$hex>" // Zmieniamy &#FF0000 na <#FF0000>
+        }
+    }
+
+    private fun formatMixedTextToMiniMessage(message: String): Component {
+        var formattedMessage = convertLegacyToMiniMessage(message)
+        formattedMessage = convertHexToMiniMessage(formattedMessage)
+
+        return MiniMessage.miniMessage().deserialize(formattedMessage)
+    }
 }
