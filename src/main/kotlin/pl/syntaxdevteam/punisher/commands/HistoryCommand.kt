6a478169c832526e748a21945574d1b986f63b2f
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-02 19:44:22 +0100

    Fixed issue with displaying results in console

--- src/main/kotlin/pl/syntaxdevteam/punisher/commands/HistoryCommand.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/commands/HistoryCommand.kt
@@ -2,7 +2,6 @@ package pl.syntaxdevteam.punisher.commands
 
 import io.papermc.paper.command.brigadier.BasicCommand
 import io.papermc.paper.command.brigadier.CommandSourceStack
-import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.jetbrains.annotations.NotNull
 import pl.syntaxdevteam.punisher.PunisherX
@@ -16,87 +15,87 @@ class HistoryCommand(private val plugin: PunisherX, private val playerIPManager:
     private val dateFormat = SimpleDateFormat("yy-MM-dd HH:mm:ss")
 
     override fun execute(@NotNull stack: CommandSourceStack, @NotNull args: Array<String>) {
-        if(stack.sender.hasPermission("punisherx.history")) {
-            if (args.isNotEmpty()) {
-                val player = args[0]
-                val page = if (args.size > 1) args[1].toIntOrNull() ?: 1 else 1
-                val senderName = stack.sender.name
-                val limit = 10
-                val offset = (page - 1) * limit
 
-                if (player.equals(senderName, ignoreCase = true)) {
-                    val uuid = plugin.uuidManager.getUUID(player)
-                    val targetPlayer = when (Bukkit.getPlayer(player)?.name) {
-                        null -> Bukkit.getOfflinePlayer(uuid).name
-                        else -> Bukkit.getPlayer(player)?.name
-                    }
-                    val punishments = plugin.databaseHandler.getPunishmentHistory(uuid.toString(), limit, offset)
+        if (args.isEmpty()) {
+            stack.sender.sendMessage(plugin.messageHandler.getMessage("history", "usage"))
+            return
+        }
 
-                    if (punishments.isEmpty()) {
-                        stack.sender.sendRichMessage(
-                            plugin.messageHandler.getMessage(
-                                "history",
-                                "no_punishments",
-                                mapOf("player" to player)
-                            )
-                        )
-                    } else {
-                        val id = plugin.messageHandler.getLogMessage("history", "id")
-                        val types = plugin.messageHandler.getLogMessage("history", "type")
-                        val reasons = plugin.messageHandler.getLogMessage("history", "reason")
-                        val times = plugin.messageHandler.getLogMessage("history", "time")
-                        val title = plugin.messageHandler.getLogMessage("history", "title")
-                        val playerIP = playerIPManager.getPlayerIPByName(player)
-                        plugin.logger.debug("Player IP: $playerIP")
-                        val geoLocation = playerIP?.let { ip ->
-                            val country = playerIPManager.geoIPHandler.getCountry(ip)
-                            val city = playerIPManager.geoIPHandler.getCity(ip)
-                            plugin.logger.debug("Country: $country, City: $city")
-                            "$city, $country"
-                        } ?: "Unknown location"
-                        plugin.logger.debug("GeoLocation: $geoLocation")
-                        val fullGeoLocation = when (stack.sender.hasPermission("punisherx.view_ip")) {
-                            true -> "$playerIP ($geoLocation)"
-                            else -> geoLocation
-                        }
-                        val gamer = if (stack.sender.name == "CONSOLE") {
-                            "<gold>$targetPlayer <gray>[$uuid, $fullGeoLocation]</gray>:</gold>"
-                        } else {
-                            "<gold><hover:show_text:'[<white>$uuid, $fullGeoLocation</white>]'>$targetPlayer:</gold>"
-                        }
-                        val miniMessage = MiniMessage.miniMessage()
-                        val topHeader =
-                            miniMessage.deserialize("<blue>--------------------------------------------------</blue>")
-                        val header = miniMessage.deserialize("<blue>|    $title $gamer</blue>")
-                        val tableHeader = miniMessage.deserialize("<blue>|   $id  |  $types  |  $reasons  |  $times</blue>")
-                        val br = miniMessage.deserialize("<blue> </blue>")
-                        val hr = miniMessage.deserialize("<blue>|</blue>")
-                        stack.sender.sendMessage(br)
-                        stack.sender.sendMessage(header)
-                        stack.sender.sendMessage(topHeader)
-                        stack.sender.sendMessage(tableHeader)
-                        stack.sender.sendMessage(hr)
+        val player = args[0]
+        if (player.equals(stack.sender.name, ignoreCase = true) || stack.sender.hasPermission("punisherx.history")) {
 
-                        punishments.forEach { punishment ->
-                            val formattedDate = dateFormat.format(Date(punishment.start))
-                            val punishmentMessage =
-                                miniMessage.deserialize("<blue>|   <white>#${punishment.id}</white> <blue>|</blue> <white>${punishment.type}</white> <blue>|</blue> <white>${punishment.reason}</white> <blue>|</blue> <white>$formattedDate</blue>")
-                            stack.sender.sendMessage(punishmentMessage)
-                        }
-                        stack.sender.sendMessage(hr)
+            val page = if (args.size > 1) args[1].toIntOrNull() ?: 1 else 1
+            val limit = 10
+            val offset = (page - 1) * limit
 
-                        val nextPage = page + 1
-                        val prevPage = if (page > 1) page - 1 else 1
-                        val navigation =
-                            miniMessage.deserialize("<blue>| <click:run_command:'/history $player $prevPage'>[Previous]</click>   <click:run_command:'/history $player $nextPage'>[Next]</click> </blue>")
-                        stack.sender.sendMessage(navigation)
-                    }
-                }
+            val uuid = plugin.uuidManager.getUUID(player)
+            val targetPlayer = when (Bukkit.getPlayer(player)?.name) {
+                null -> Bukkit.getOfflinePlayer(uuid).name
+                else -> Bukkit.getPlayer(player)?.name
+            }
+            val punishments = plugin.databaseHandler.getPunishmentHistory(uuid.toString(), limit, offset)
+
+            if (punishments.isEmpty()) {
+                stack.sender.sendMessage(
+                    plugin.messageHandler.getMessage(
+                        "history",
+                        "no_punishments",
+                        mapOf("player" to player)
+                    )
+                )
             } else {
-                stack.sender.sendRichMessage(plugin.messageHandler.getMessage("history", "usage"))
+                val id = plugin.messageHandler.getCleanMessage("history", "id")
+                val types = plugin.messageHandler.getCleanMessage("history", "type")
+                val reasons = plugin.messageHandler.getCleanMessage("history", "reason")
+                val times = plugin.messageHandler.getCleanMessage("history", "time")
+                val title = plugin.messageHandler.getCleanMessage("history", "title")
+                val playerIP = playerIPManager.getPlayerIPByName(player)
+                plugin.logger.debug("Player IP: $playerIP")
+                val geoLocation = playerIP?.let { ip ->
+                    val country = playerIPManager.geoIPHandler.getCountry(ip)
+                    val city = playerIPManager.geoIPHandler.getCity(ip)
+                    plugin.logger.debug("Country: $country, City: $city")
+                    "$city, $country"
+                } ?: "Unknown location"
+                plugin.logger.debug("GeoLocation: $geoLocation")
+                val fullGeoLocation = when (stack.sender.hasPermission("punisherx.view_ip")) {
+                    true -> "$playerIP ($geoLocation)"
+                    else -> geoLocation
+                }
+                val gamer = if (stack.sender.name == "CONSOLE") {
+                    "<gold>$targetPlayer <gray>[$uuid, $fullGeoLocation]</gray>:</gold>"
+                } else {
+                    "<gold><hover:show_text:'[<white>$uuid, $fullGeoLocation</white>]'>$targetPlayer:</gold>"
+                }
+                val mh = plugin.messageHandler
+                val topHeader =
+                    mh.miniMessageFormat("<blue>--------------------------------------------------</blue>")
+                val header = mh.miniMessageFormat("<blue>|    $title $gamer</blue>")
+                val tableHeader = mh.miniMessageFormat("<blue>|   $id  |  $types  |  $reasons  |  $times</blue>")
+                val br = mh.miniMessageFormat("<blue> </blue>")
+                val hr = mh.miniMessageFormat("<blue>|</blue>")
+                stack.sender.sendMessage(br)
+                stack.sender.sendMessage(header)
+                stack.sender.sendMessage(topHeader)
+                stack.sender.sendMessage(tableHeader)
+                stack.sender.sendMessage(hr)
+
+                punishments.forEach { punishment ->
+                    val formattedDate = dateFormat.format(Date(punishment.start))
+                    val punishmentMessage =
+                        mh.miniMessageFormat("<blue>|   <white>#${punishment.id}</white> <blue>|</blue> <white>${punishment.type}</white> <blue>|</blue> <white>${punishment.reason}</white> <blue>|</blue> <white>$formattedDate</blue>")
+                    stack.sender.sendMessage(punishmentMessage)
+                }
+                stack.sender.sendMessage(hr)
+
+                val nextPage = page + 1
+                val prevPage = if (page > 1) page - 1 else 1
+                val navigation =
+                    mh.miniMessageFormat("<blue>| <click:run_command:'/history $player $prevPage'>[Previous]</click>   <click:run_command:'/history $player $nextPage'>[Next]</click> </blue>")
+                stack.sender.sendMessage(navigation)
             }
         } else {
-            stack.sender.sendRichMessage(plugin.messageHandler.getMessage("history", "no_permission"))
+            stack.sender.sendMessage(plugin.messageHandler.getMessage("history", "no_permission"))
         }
     }
 
