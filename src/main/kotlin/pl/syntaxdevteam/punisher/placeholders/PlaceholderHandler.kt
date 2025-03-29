9e0a4c62a52395a5455281ef97faf00f392fd69f
SyntaxDevTeam <wieszczy85@gmail.com>
2025-03-29 11:35:51 +0100

    Fixed all placeholders

--- src/main/kotlin/pl/syntaxdevteam/punisher/placeholders/PlaceholderHandler.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/placeholders/PlaceholderHandler.kt
@@ -3,7 +3,6 @@ package pl.syntaxdevteam.punisher.placeholders
 import org.bukkit.entity.Player
 import pl.syntaxdevteam.punisher.PunisherX
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
-import java.util.concurrent.CompletableFuture
 
 @Suppress("UnstableApiUsage")
 class PlaceholderHandler(private val plugin: PunisherX) : PlaceholderExpansion() {
@@ -26,55 +25,63 @@ class PlaceholderHandler(private val plugin: PunisherX) : PlaceholderExpansion()
         }
 
         return when (params) {
-            "mute_remaining_time" -> getMuteEndTime(player.name).getNow("")
-            "warn_remaining_time" -> getWarnEndTime(player.name).getNow("")
-            "total_punishments" -> getAllPunishmentHistory().getNow("")
-            "total_active_punishments" -> getAllPunishments().getNow("")
+            "mute_remaining_time" -> getMuteEndTime(player.name) ?: ""
+            "warn_remaining_time" -> getWarnEndTime(player.name) ?: ""
+            "total_active_punishments" -> getAllPunishments() ?: ""
+            "total_punishments" -> getAllPunishmentHistory() ?: ""
             else -> null
         }
     }
 
-    private fun getMuteEndTime(player: String): CompletableFuture<String?> {
-        return CompletableFuture.supplyAsync {
-            val uuid = plugin.uuidManager.getUUID(player)
-            val punishments = plugin.databaseHandler.getPunishments(uuid.toString())
-            val muteData = punishments.find { it.type == "MUTE" && it.end > System.currentTimeMillis() } ?: return@supplyAsync null
+    private fun getMuteEndTime(player: String): String? {
+        val uuid = plugin.uuidManager.getUUID(player)
+        val punishments = plugin.databaseHandler.getPunishments(uuid.toString())
+        val muteData = punishments.find { it.type == "MUTE" && it.end > System.currentTimeMillis() } ?: return null
 
-            val remainingTime = (muteData.end - System.currentTimeMillis()) / 1000
-            if (remainingTime > 0) {
-                plugin.messageHandler.getCleanMessage("placeholders", "mute_remaining_time") + plugin.timeHandler.formatTime(remainingTime.toString())
-            } else {
-                null
+        val remainingTime = (muteData.end - System.currentTimeMillis())  / 1000
+        return if (remainingTime > 0) {
+            plugin.messageHandler.getCleanMessage("placeholders", "mute_remaining_time") + plugin.timeHandler.formatTime(remainingTime.toString())
+        } else {
+            null
+        }
+    }
+
+    private fun getWarnEndTime(player: String): String? {
+        val uuid = plugin.uuidManager.getUUID(player)
+        val punishments = plugin.databaseHandler.getPunishments(uuid.toString())
+        val muteData = punishments.find { it.type == "WARN" && it.end > System.currentTimeMillis() } ?: return null
+
+        val remainingTime = (muteData.end - System.currentTimeMillis())  / 1000
+        return if (remainingTime > 0) {
+            plugin.messageHandler.getCleanMessage("placeholders", "warn_remaining_time") + plugin.timeHandler.formatTime(remainingTime.toString())
+        } else {
+            null
+        }
+    }
+
+    private fun getAllPunishments(): String? {
+        val totalPunishments = plugin.databaseHandler.countAllPunishments()
+        return if (totalPunishments > 0) {
+        plugin.logger.debug("Total active punishments: $totalPunishments")
+        plugin.logger.debug("Formatting message for total active punishments: ${plugin.messageHandler.getCleanMessage("placeholders", "total_active_punishments")}")
+        plugin.messageHandler.getCleanMessage("placeholders", "total_active_punishments") + totalPunishments.toString()
+        } else {
+            null
+        }
+
+    }
+
+    private fun getAllPunishmentHistory(): String? {
+
+        val totalPunishmentHistory = plugin.databaseHandler.countAllPunishmentHistory()
+        return if (totalPunishmentHistory > 0) {
+            plugin.logger.debug("Total punishments: $totalPunishmentHistory")
+            plugin.logger.debug("Formatting message for total punishments: ${plugin.messageHandler.getCleanMessage("placeholders", "total_punishments")}")
+            plugin.messageHandler.getCleanMessage("placeholders", "total_punishments").let {
+                it + totalPunishmentHistory.toString()
             }
-        }
-    }
-
-    private fun getWarnEndTime(player: String): CompletableFuture<String?> {
-        return CompletableFuture.supplyAsync {
-            val uuid = plugin.uuidManager.getUUID(player)
-            val punishments = plugin.databaseHandler.getPunishments(uuid.toString())
-            val warnData = punishments.find { it.type == "WARN" && it.end > System.currentTimeMillis() } ?: return@supplyAsync null
-
-            val remainingTime = (warnData.end - System.currentTimeMillis()) / 1000
-            if (remainingTime > 0) {
-                plugin.messageHandler.getCleanMessage("placeholders", "warn_remaining_time") + plugin.timeHandler.formatTime(remainingTime.toString())
-            } else {
-                null
-            }
-        }
-    }
-
-    private fun getAllPunishments(): CompletableFuture<String?> {
-        return CompletableFuture.supplyAsync {
-            val totalPunishments = plugin.databaseHandler.countAllPunishments()
-            plugin.messageHandler.getCleanMessage("placeholders", "total_active_punishments") + totalPunishments.toString()
-        }
-    }
-
-    private fun getAllPunishmentHistory(): CompletableFuture<String?> {
-        return CompletableFuture.supplyAsync {
-            val totalPunishmentHistory = plugin.databaseHandler.countAllPunishmentHistory()
-            plugin.messageHandler.getCleanMessage("placeholders", "total_punishments") + totalPunishmentHistory.toString()
+        } else {
+            null
         }
     }
 }
