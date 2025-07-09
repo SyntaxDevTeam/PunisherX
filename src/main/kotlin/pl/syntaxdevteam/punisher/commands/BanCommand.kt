5dca75fd65d21afdefc54cf7604391ec9271472a
WieszczY <wieszczy85@gmail.com>
2025-07-09 09:20:42 +0200

    Hotfix: stack.sender as Player

--- src/main/kotlin/pl/syntaxdevteam/punisher/commands/BanCommand.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/commands/BanCommand.kt
@@ -15,8 +15,8 @@ class BanCommand(private var plugin: PunisherX) : BasicCommand {
     private val clp = plugin.commandLoggerPlugin
 
     override fun execute(@NotNull stack: CommandSourceStack, @NotNull args: Array<String>) {
-
-        if (PermissionChecker.hasWithLegacy(stack.sender, PermissionChecker.PermissionKey.BAN)) {
+    val sender = stack.source().bukkitSender
+        if (!PermissionChecker.hasWithLegacy(sender, PermissionKey.BAN)) {
             if (args.isNotEmpty()) {
                 if (args.size < 2) {
                     stack.sender.sendMessage(plugin.messageHandler.getMessage("ban", "usage"))
