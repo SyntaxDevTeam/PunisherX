e89a08b63ecb963cf3d641354909324eb343a090
SyntaxDevTeam <wieszczy85@gmail.com>
2025-09-17 20:47:13 +0200

    Minor corrections

--- src/main/kotlin/pl/syntaxdevteam/punisher/loader/SyntaxLoader.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/loader/SyntaxLoader.kt
@@ -19,12 +19,12 @@ class SyntaxLoader : PluginLoader {
         resolveLibraries().map { DefaultArtifact(it) }
             .forEach { artifact -> resolver.addDependency(Dependency(artifact, null)) }
 
+        resolver.addRepository(RemoteRepository.Builder("syntaxdevteam", "default", "https://nexus.syntaxdevteam.pl/repository/maven-snapshots/").build())
+        resolver.addRepository(RemoteRepository.Builder("syntaxdevteam", "default", "https://nexus.syntaxdevteam.pl/repository/maven-releases/").build())
         resolver.addRepository(RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build())
         resolver.addRepository(RemoteRepository.Builder("central-mirror", "default", "https://repo.papermc.io/repository/maven-central/").build())
         resolver.addRepository(RemoteRepository.Builder("jitpack", "default", "https://jitpack.io/").build())
         resolver.addRepository(RemoteRepository.Builder("codemc-snapshots", "default", "https://repo.codemc.io/repository/maven-snapshots/").build())
-        resolver.addRepository(RemoteRepository.Builder("syntaxdevteam", "default", "https://nexus.syntaxdevteam.pl/repository/maven-snapshots/").build())
-        resolver.addRepository(RemoteRepository.Builder("syntaxdevteam", "default", "https://nexus.syntaxdevteam.pl/repository/maven-releases/").build())
         pluginClasspath.addLibrary(resolver)
     }
 
