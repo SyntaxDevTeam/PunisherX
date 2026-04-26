b733cd6dc5e407b997717326c904e4615fc89c8b
SyntaxDevTeam <wieszczy85@gmail.com>
2026-04-26 14:26:27 +0200

    Update syntaxgui-api dependency to version 0.1.1 and clean up command registrations

--- src/main/kotlin/pl/syntaxdevteam/punisher/commands/CommandManager.kt
+++ src/main/kotlin/pl/syntaxdevteam/punisher/commands/CommandManager.kt
@@ -115,12 +115,12 @@ class CommandManager(private val plugin: PunisherX) {
                 "clearall",
                 plugin.messageHandler.stringMessageToString("clear", "usage"),
                 ClearAllCommand(plugin)
-            )/*
+            )
             commands.register(
                 "panel",
                 "Opens the PunisherX GUI with lots of useful information and commands.",
                 PanelCommand(plugin)
-            )*/
+            )
             commands.register(
                 "langfix",
                 "Converts legacy translation placeholders from {} to <>",
@@ -158,12 +158,12 @@ class CommandManager(private val plugin: PunisherX) {
                         plugin.messageHandler.stringMessageToString("warn", "usage"),
                         WarnCommand(plugin)
                     )
-/*
+
                     "punish" -> commands.register(
                         commandName,
                         plugin.messageHandler.stringMessageToString("punish", "usage"),
                         PunishCommand(plugin)
-                    )*/
+                    )
 
                     "unwarn" -> commands.register(
                         commandName,
@@ -235,11 +235,11 @@ class CommandManager(private val plugin: PunisherX) {
                         plugin.messageHandler.stringMessageToString("clear", "usage"),
                         ClearAllCommand(plugin)
                     )
-                    /*"panel" -> commands.register(
+                    "panel" -> commands.register(
                         commandName,
                         "Opens the PunisherX GUI with lots of useful information and commands.",
                         PanelCommand(plugin)
-                    )*/
+                    )
                 }
             }
         }
